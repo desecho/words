673d324915df51c35f48103f62aa9b14c3b205a2
Anton Samarchyan <desecho@gmail.com>
2026-05-09 11:22:05 -0400

    Fix prod commands

--- scripts/run_management_command_interactive_prod.sh
+++ scripts/run_management_command_interactive_prod.sh
@@ -2,5 +2,6 @@
 
 set -eou pipefail
 
+BACKEND_CONTAINER=${BACKEND_CONTAINER:-words}
 POD_ID=$(kubectl get pods -lapp="$PROJECT" | grep Running | awk '{print $1}')
-kubectl exec -ti "$POD_ID" -- ./src/manage.py "$@"
+kubectl exec -ti "$POD_ID" --container "$BACKEND_CONTAINER" -- ./src/manage.py "$@"
--- scripts/run_management_command_prod.sh
+++ scripts/run_management_command_prod.sh
@@ -2,5 +2,6 @@
 
 set -eou pipefail
 
+BACKEND_CONTAINER=${BACKEND_CONTAINER:-words}
 POD_ID=$(kubectl get pods -lapp="$PROJECT" | grep Running | awk '{print $1}')
-kubectl exec "$POD_ID" -- ./src/manage.py "$@"
+kubectl exec "$POD_ID" --container "$BACKEND_CONTAINER" -- ./src/manage.py "$@"
